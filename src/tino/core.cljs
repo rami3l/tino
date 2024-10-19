baea0cd72515ac2cf0539520a0e5ac0f4426d6d0
rami3l <rami3l@outlook.com>
2024-10-19 10:02:56 +0800

    feat: add skeleton for using `tio.js`

--- src/tino/core.cljs
+++ src/tino/core.cljs
@@ -1,3 +1,11 @@
-(ns tino.core)
+(ns tino.core
+  (:require [shadow.cljs.modern :refer (js-await)]
+            ;; About the import trick below:
+            ;; <https://gist.github.com/borkdude/7e548f06fbefeb210f3fcf14eef019e0>
+            ["tio.js$default" :as tio]))
 
-(defn main [& cli-args] (prn "hello world"))
+(defn main [& cli-args] (prn (.-languages tio)))
+
+;; (defn tio-eval [code]
+;;   (let [
+;;         response (<p! )]))
