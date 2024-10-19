f6899cc5c7852cf0372aee15037d0e092b4c9a36
rami3l <rami3l@outlook.com>
2024-10-19 12:48:34 +0800

    feat: impl simple tio eval on Telegram

--- index.js
+++ index.js
@@ -1,3 +1,73 @@
+import { SocksProxyAgent } from "socks-proxy-agent";
+import { Telegraf } from "telegraf";
 import { default as tio } from "tio.js";
+import "dotenv/config";
 
-console.log(tio.languages);
+const botToken = process.env.TINO_TELEGRAM_BOT_TOKEN;
+if (!botToken) {
+  console.error("ERROR: `TINO_TELEGRAM_BOT_TOKEN` is missing");
+  process.exit(1);
+}
+
+const botNewOpts = {};
+const socksProxy = process.env.SOCKS_PROXY;
+if (socksProxy) {
+  console.error(`Using SOCKS proxy: ${socksProxy}`);
+  botNewOpts.telegram = { agent: new SocksProxyAgent(socksProxy) };
+}
+const bot = new Telegraf(botToken, botNewOpts);
+
+var botWebhookOpts = null;
+const botWebhookListen = process.env.TINO_TELEGRAM_BOT_WEBHOOK_LISTEN;
+if (botWebhookListen) {
+  botWebhookOpts = { path: botWebhookListen, port: process.env.PORT || 443 };
+  console.error(`Running in webhook mode at \`${botWebhookListen}\``);
+}
+
+/** @type {string[]} */
+const langs = tio.languages;
+
+async function showLangs(ctx) {
+  await ctx.reply(
+    "Please refer to https://github.com/TryItOnline/tiosetup/tree/master/languages for a list of supported languages.",
+    { reply_to_message_id: ctx.message.message_id }
+  );
+}
+
+bot.command("tio", async (ctx) => {
+  console.error("Triggered /tio");
+  await showLangs(ctx);
+});
+
+langs.forEach((lang) => {
+  let cmd = "tio" + lang;
+  bot.command(cmd, async (ctx) => {
+    console.error(`Triggered /${cmd}`);
+    const args = ctx.message.text.split(/ (.*)/s);
+    if (args.length < 2) {
+      await ctx.reply("Usage: /tio<lang> <code>", {
+        reply_to_message_id: ctx.message.message_id,
+      });
+      return;
+    }
+    if (!langs.includes(lang)) {
+      await showLangs(ctx);
+      return;
+    }
+    const [, code] = args;
+    const { output, exitCode, realTime } = await tio(code, { language: lang });
+    await ctx.reply(`${output}[exit(${exitCode}) in ${realTime}s]`, {
+      reply_to_message_id: ctx.message.message_id,
+    });
+  });
+});
+
+const botLaunchOpts = {};
+if (botWebhookOpts) {
+  botLaunchOpts.webhook = botWebhookOpts;
+}
+bot.launch(botLaunchOpts);
+console.log("The bot is now running...");
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
